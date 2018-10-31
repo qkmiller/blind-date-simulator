30a8b9286e06b64908c174b008b70b6d35fc97d2
Nicole Cox <nicolecox@Nicoles-MacBook-Pro.local>
2018-10-30 21:30:34 -0700

    Adding in click functions for questions 4-15 in order

--- js/scripts.js
+++ js/scripts.js
@@ -27,4 +27,76 @@ $(document).ready(function(){
     $("#frame2").hide();
     $("#frame4").show();
   });
+  $("#frame4 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame4").hide();
+    $("#frame5").show();
+  });
+  $("#frame5 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame5").hide();
+    $("#frame6").show();
+  });
+  $("#frame6 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame6").hide();
+    $("#frame7").show();
+  });
+  $("#frame7 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame7").hide();
+    $("#frame8").show();
+  });
+  $("#frame8 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame8").hide();
+    $("#frame9").show();
+  });
+  $("#frame9 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame9").hide();
+    $("#frame10").show();
+  });
+  $("#frame10 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame10").hide();
+    $("#frame11").show();
+  });
+  $("#frame11 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame11").hide();
+    $("#frame12").show();
+  });
+  $("#frame12 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame12").hide();
+    $("#frame13").show();
+  });
+  $("#frame13 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame13").hide();
+    $("#frame14").show();
+  });
+  $("#frame14 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame13").hide();
+    $("#frame14").show();
+  });
+  $("#frame15 button").click(function(){
+    name = $("input[type=text]").val();
+    $(".username").text(name);
+    $("#frame15").hide();
+    $("#frame15").show();
+  });
 });
